8ef5edb75520f705e2f9c3531d12cdfe6dc19fc6
alvariseppanen <110089195+alvariseppanen@users.noreply.github.com>
2022-11-18 09:04:00 +0200

    Delete scripts directory

--- scripts/setup.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import numpy as np
-from distutils.core import setup
-from Cython.Build import cythonize
-
-include_dirs=[np.get_include()]
-
-setup(ext_modules = cythonize('custom_functions.pyx',compiler_directives={'language_level' : "3"}), include_dirs=[np.get_include()])
